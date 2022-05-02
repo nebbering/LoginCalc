2d968a797784eae4a003d0705cbc4270a9c3a751
Luan H. Nebbering <78267195+nebbering@users.noreply.github.com>
2022-05-02 11:11:43 -0300

    up

--- Calculadora.vb
+++ Calculadora.vb
@@ -2,4 +2,16 @@
     Private Sub Calculadora_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+
+    Private Sub btn0_Click(sender As Object, e As EventArgs) Handles btn0.Click
+        txtResultado.Text = "0"
+    End Sub
+
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        txtResultado.Text = "1"
+    End Sub
+
+    Private Sub btnC_Click(sender As Object, e As EventArgs) Handles btnC.Click
+        txtResultado.Text = "0"
+    End Sub
 End Class
