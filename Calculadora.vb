f0859d1d4de4a127557ad318a1e0530e165ee824
Luan H. Nebbering <78267195+nebbering@users.noreply.github.com>
2022-05-04 13:45:41 -0300

    Update

--- Calculadora.vb
+++ Calculadora.vb
@@ -6,13 +6,15 @@
     Private operacao As String
     Private resultado As String
     Private valor As Double
-    Private SeOperador As Boolean = False
+    Private Operador As Boolean = False
 
     Private Sub btn0_Click(sender As Object, e As EventArgs) Handles btn0.Click
+        Concatenar()
         txtResultado.Text = txtResultado.Text + "0"
     End Sub
 
     Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        Concatenar()
         txtResultado.Text = txtResultado.Text + "1"
     End Sub
 
@@ -21,34 +23,42 @@
     End Sub
 
     Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        Concatenar()
         txtResultado.Text = txtResultado.Text + "2"
     End Sub
 
     Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        Concatenar()
         txtResultado.Text = txtResultado.Text + "3"
     End Sub
 
     Private Sub btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+        Concatenar()
         txtResultado.Text = txtResultado.Text + "4"
     End Sub
 
     Private Sub btn5_Click(sender As Object, e As EventArgs) Handles btn5.Click
+        Concatenar()
         txtResultado.Text = txtResultado.Text + "5"
     End Sub
 
     Private Sub btn6_Click(sender As Object, e As EventArgs) Handles btn6.Click
+        Concatenar()
         txtResultado.Text = txtResultado.Text + "6"
     End Sub
 
     Private Sub btn7_Click(sender As Object, e As EventArgs) Handles btn7.Click
+        Concatenar()
         txtResultado.Text = txtResultado.Text + "7"
     End Sub
 
     Private Sub btn8_Click(sender As Object, e As EventArgs) Handles btn8.Click
+        Concatenar()
         txtResultado.Text = txtResultado.Text + "8"
     End Sub
 
     Private Sub btn9_Click(sender As Object, e As EventArgs) Handles btn9.Click
+        Concatenar()
         txtResultado.Text = txtResultado.Text + "9"
     End Sub
 
@@ -81,11 +91,11 @@
     End Sub
 
     Private Sub btnIgual_Click(sender As Object, e As EventArgs) Handles btnIgual.Click
-
+        '        txtResultado.Text = 
     End Sub
 
     Public Sub AvaliaEFazOperacao()
-        If SeOperador = True Then
+        If Operador = True Then
             valor = txtResultado.Text
 
             Select Case operacao
@@ -105,6 +115,20 @@
 
         End If
 
+    End Sub
+
+    Public Sub Concatenar()
+
+        If Operador = True Then
+
+            txtResultado.Text = ""
+            Operador = False
+
+        ElseIf txtResultado.Text = "0" Then
+
+            txtResultado.Text = ""
+
+        End If
     End Sub
 
 End Class
